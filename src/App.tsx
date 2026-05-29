277a54e743b2cb0eb61c3c470843969cbecb8bcc
Antoine Alejandro <antoine.alejandro@vis-mobility.com>
2026-05-29 15:49:39 +0200

    remove

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import HistoryPage from './history/HistoryPage';
-
-function App() {
-  return <HistoryPage />;
-}
-
-export default App;
--- src/history/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Introduction } from './Introduction';
-export { default as Timeline } from './Timeline';
-export { default as Milestones } from './Milestones';
-export { default as FutureInitiatives } from './FutureInitiatives';
